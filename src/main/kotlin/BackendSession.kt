2094a0792396bf481eed5ee615a7c9916a97165b
aleker <aleksandra.kerebinska@student.put.poznan.pl>
2018-12-15 12:22:20 +0100

    Moving methods into new lines.

--- src/main/kotlin/BackendSession.kt
+++ src/main/kotlin/BackendSession.kt
@@ -22,6 +22,7 @@ typealias SeatNo = Int
 class BackendSession(contactPoint: String, keyspace: String) {
 
     private val session: Session
+
     init {
         val cluster = Cluster.builder().addContactPoint(contactPoint).build()
         session = cluster.connect(keyspace)
@@ -36,32 +37,42 @@ class BackendSession(contactPoint: String, keyspace: String) {
     private val GET_FREE_SEATS_BY_FLIGHT by lazy { session.prepare("SELECT * FROM seats WHERE flight_id = ? AND is_free = true;") }
 
     fun isSeatFree(flightId: FlightId, seatNo: SeatNo): Boolean {
-        val result = session.execute(BoundStatement(IS_SEAT_FREE).bind(flightId, seatNo)).map { r -> r.getBool("is_free") }
+        val result = session
+            .execute(BoundStatement(IS_SEAT_FREE).bind(flightId, seatNo))
+            .map { r -> r.getBool("is_free") }
         logger.info(IS_SEAT_FREE.toString())
         return if (result.size == 1) result[0] else throw RuntimeException("Should not happen")
     }
 
     fun getFlights(day: String, departure: String): List<Flight> {
-        val result =  session.execute(BoundStatement(GET_FLIGHTS_BY_DAY_AND_DEPARTURE).bind(departure, day)).map { r -> Flight(r) }
+        val result = session
+            .execute(BoundStatement(GET_FLIGHTS_BY_DAY_AND_DEPARTURE).bind(departure, day))
+            .map { r -> Flight(r) }
         logger.info(GET_FLIGHTS_BY_DAY_AND_DEPARTURE.toString())
         return result
     }
 
     fun getFreeSeatsCount(flightId: FlightId): Int {
-        val result = session.execute(BoundStatement(GET_FREE_SEATS_COUNT_BY_FLIGHT).bind(flightId)).map { r -> r.getInt("count") }
+        val result = session
+            .execute(BoundStatement(GET_FREE_SEATS_COUNT_BY_FLIGHT).bind(flightId))
+            .map { r -> r.getInt("count") }
         logger.info(GET_FREE_SEATS_COUNT_BY_FLIGHT.toString())
         return if (result.size == 1) result[0] else throw RuntimeException("Should not happen")
     }
 
     fun getFreeSeats(flightId: FlightId): List<Seat> {
-        val result = session.execute(BoundStatement(GET_FREE_SEATS_BY_FLIGHT).bind(flightId)).map { r -> Seat(r) }
+        val result = session
+            .execute(BoundStatement(GET_FREE_SEATS_BY_FLIGHT).bind(flightId))
+            .map { r -> Seat(r) }
         logger.info(GET_FREE_SEATS_BY_FLIGHT.toString())
         return result
     }
 
     // Probably will be unused
     fun getAllReservations(): List<Reservation> {
-        val result = session.execute(BoundStatement(GET_ALL_RESERVATIONS)).map { r -> Reservation(r) }
+        val result = session
+            .execute(BoundStatement(GET_ALL_RESERVATIONS))
+            .map { r -> Reservation(r) }
         logger.info(GET_ALL_RESERVATIONS.toString())
         return result
     }
