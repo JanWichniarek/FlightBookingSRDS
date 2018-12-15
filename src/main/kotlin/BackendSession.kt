d0ebed7c4a5e3c889077b38c02373e4dd0a6f061
aleker <aleksandra.kerebinska@student.put.poznan.pl>
2018-12-15 11:00:29 +0100

    allow filtering removed from query. Adding index on is_free in seats.

--- src/main/kotlin/BackendSession.kt
+++ src/main/kotlin/BackendSession.kt
@@ -31,7 +31,7 @@ class BackendSession(contactPoint: String, keyspace: String) {
     private val IS_SEAT_FREE by lazy { session.prepare("SELECT is_free FROM seats WHERE flight_id = ? AND seat_no = ?") }
     private val GET_FLIGHTS_BY_DAY_AND_DEPARTURE by lazy { session.prepare("SELECT * FROM flights WHERE departure = '?' AND date = '?'") }
     private val GET_FREE_SEATS_COUNT_BY_FLIGHT by lazy { session.prepare("SELECT count FROM free_seats WHERE flight_id = ?") }
-    private val GET_FREE_SEATS_BY_FLIGHT by lazy { session.prepare("SELECT * FROM seats WHERE flight_id = ? AND is_free = true allow filtering;") }
+    private val GET_FREE_SEATS_BY_FLIGHT by lazy { session.prepare("SELECT * FROM seats WHERE flight_id = ? AND is_free = true") }
 
     fun isSeatFree(flightId: FlightId, seatNo: SeatNo): Boolean {
         val result = session.execute(BoundStatement(IS_SEAT_FREE).bind(flightId, seatNo)).map { r -> r.getBool("is_free") }
